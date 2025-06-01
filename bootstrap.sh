439a2457d95b245b295805b919f1f14b71979471
Michael Rose <michael.rose0@gmail.com>
2025-05-31 18:34:48 -0700

    Fix up bootstrap to use git rather than curl'ing another file

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,36 +1,59 @@
 #! /bin/bash
 
+install_brew() {
+    if which brew >/dev/null 2>&1; then
+        echo -e "\nbrew already installed. Skipping.\n"
+    else
+        echo -e "\nInstalling Homebrew ...\n"
+        bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    fi
+}
+
 REPO_SRC="https://raw.githubusercontent.com/miker2/computer_setup/refs/heads/master"
 
 # Get the OS type. Linux and MacOS are supported
-OS_TYPE=$(uname -o)
+OS_TYPE=$(uname)
 ARCH_TYPE=$(uname -p)
 case ${OS_TYPE} in
-	"GNU/Linux")
-		SETUP_FILE="linux_setup.sh"
-		if [ ${ARCH_TYPE} == "x86_64"
-			SETUP_FILE="linux_setup.sh"
-		else
-			# We assume here that an aarch64 device is a raspberry pi
-			# Maybe not the right thing to do (could be a Jetson as well)
-			# We could use '/sys/firmware/devicetree/base/model' perhaps
-			# if it exists to see if it is a pi. 
-			# We may not actually care here about the difference between
-			# A raspberry pi running linux and a normal linux computer
-			SETUP_FILE="rpi_setup.sh"
-		end
+    "Linux")
+        SETUP_FILE="linux_setup.sh"
+        sudo apt-get update
+        sudo apt-get install -y git
+        ;;
+    "Darwin")
+        SETUP_FILE="mac_setup.sh"
+		install_brew
+		brew install git
+		brew install curl
+        ;;
+    *)
+        echo "Unsupported OS type: ${OS_TYPE}"
+        exit 1
 		;;
-	"Darwin")
-		SETUP_FILE="mac_setup.sh"
-		;;
-	*)
-		echo "Unsupported OS type: ${OS_TYPE}"
-		exit 1
+esac
 
-curl -o /tmp/setup.sh ${REPO_SRC}/${SETUP_FILE}
+REPO="https://github.com/miker2/computer_setup.git"
+DEST="${HOME}/.dotfiles"
 
 echo "OS_TYPE=${OS_TYPE}"
 echo "ARCH_TYPE=${ARCH_TYPE}"
-echo "Running ${SETUP_FILE}"
-# bash /tmp/setup.sh
 
+if [ -d "${DEST}" ]; then
+	echo "Directory ${DEST} already exists. Updating..."
+
+	pushd ${DEST}
+		git fetch
+		git checkout master
+		git pull
+	popd
+else
+	git clone --depth 1 ${REPO} ${DEST}
+fi
+
+if [ ! -f "${DEST}/setup/${SETUP_FILE}" ]; then
+	echo "Setup file ${SETUP_FILE} not found in ${DEST}/setup/"
+	exit 1
+fi
+
+echo "Running ${DEST}/setup/${SETUP_FILE}"
+# bash /tmp/setup.sh
