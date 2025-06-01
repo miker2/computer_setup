4f64ef5f1a423756dbf22929ca64898e2c6844a0
Michael Rose <michael.rose0@gmail.com>
2025-06-01 09:56:07 -0700

    only install git if it doesn't exist already

--- bootstrap.sh
+++ bootstrap.sh
@@ -10,55 +10,59 @@ install_brew() {
 }
 
 _bootstrap_main () {
-	# Get the OS type. Linux and MacOS are supported
-	local -r OS_TYPE=$(uname)
-	local -r ARCH_TYPE=$(uname -p)
-	case ${OS_TYPE} in
-		"Linux")
-			SETUP_FILE="linux_setup.sh"
-			sudo apt-get update
-			sudo apt-get install -y git
-			;;
-		"Darwin")
-			SETUP_FILE="mac_setup.sh"
-			install_brew
-			brew install git
-			;;
-		*)
-			echo "Unsupported OS type: ${OS_TYPE}"
-			exit 1
-			;;
-	esac
+    # Get the OS type. Linux and MacOS are supported
+    local -r OS_TYPE=$(uname)
+    local -r ARCH_TYPE=$(uname -m)
+    case ${OS_TYPE} in
+        "Linux")
+            SETUP_FILE="linux_setup.sh"
+            if [[ -z "$(which git)" ]]; then
+                sudo apt-get update
+                sudo apt-get install -y git
+            fi
+            ;;
+        "Darwin")
+            SETUP_FILE="mac_setup.sh"
+            install_brew
+            if [[ -z "$(which git)" ]]; then
+                brew install git
+            fi
+            ;;
+        *)
+            echo "Unsupported OS type: ${OS_TYPE}"
+            exit 1
+            ;;
+    esac
 
-	local -r REPO="https://github.com/miker2/computer_setup.git"
-	local -r DEST="${HOME}/.dotfiles"
+    local -r REPO="https://github.com/miker2/computer_setup.git"
+    local -r DEST="${HOME}/.dotfiles"
 
-	echo "OS_TYPE=${OS_TYPE}"
-	echo "ARCH_TYPE=${ARCH_TYPE}"
+    echo "OS_TYPE=${OS_TYPE}"
+    echo "ARCH_TYPE=${ARCH_TYPE}"
 
-	if [ -d "${DEST}" ]; then
-		echo "Directory ${DEST} already exists. Updating..."
+    if [ -d "${DEST}" ]; then
+        echo "Directory ${DEST} already exists. Updating..."
 
-		pushd "${DEST}" || echo "Failed to change directory to ${DEST}" && exit 1
-			git fetch
-			git checkout master
-			git pull
-		popd || echo "Failed to change back to previous directory" && exit 1
-	else
-		git clone --depth 1 ${REPO} "${DEST}"
-	fi
+        pushd "${DEST}" || echo "Failed to change directory to ${DEST}" && exit 1
+            git fetch
+            git checkout master
+            git pull
+        popd || echo "Failed to change back to previous directory" && exit 1
+    else
+        git clone --depth 1 --recurse-submodules ${REPO} "${DEST}"
+    fi
 
-	if [ ! -f "${DEST}/setup/${SETUP_FILE}" ]; then
-		echo "Setup file ${SETUP_FILE} not found in ${DEST}/setup/"
-		exit 1
-	fi
+    if [ ! -f "${DEST}/setup/${SETUP_FILE}" ]; then
+        echo "Setup file ${SETUP_FILE} not found in ${DEST}/setup/"
+        exit 1
+    fi
 
-	echo "Running ${DEST}/setup/${SETUP_FILE}"
-	# bash "${DEST}/setup/${SETUP_FILE}" || {
-	#	echo "Failed to run setup script ${DEST}/setup/${SETUP_FILE}"
-	#	exit 1
-	# }
-	echo "Test completed successfully."
+    echo "Running ${DEST}/setup/${SETUP_FILE}"
+    # bash "${DEST}/setup/${SETUP_FILE}" || {
+    #    echo "Failed to run setup script ${DEST}/setup/${SETUP_FILE}"
+    #    exit 1
+    # }
+    echo "Test completed successfully."
 }
 
 if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
